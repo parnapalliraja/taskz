5bfc33c5808618431348ae10e9e13db2a8113e35
Rajasekhar <parnapalliraja123@gmail.com>
2022-10-12 15:31:20 +0530

    First commit task manager app

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const assert = require('assert')
+const taskRoute = require('./route/taskRoute')
+
+const port = 5000
+
+const app = express()
+
+//body parser middleware
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(cors())
+
+// view engine
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.use(`/`,taskRoute)
+
+app.all('*', (req,res)=>{
+    res.render('pnf')
+}
+)
+
+app.listen(port,()=>{
+    console.log(`server is running @ ${port}`)
+})
+
